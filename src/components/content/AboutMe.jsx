e2c013076de8ec204ffb0f9dd973a717ff26bd65
Gustavo <gustavo.henrikear@gmail.com>
2026-02-21 10:47:27 -0300

    Ajustando o Sobre Mim

--- src/components/content/AboutMe.jsx
+++ src/components/content/AboutMe.jsx
@@ -11,7 +11,7 @@ const AboutMe = () => {
         
         <div className="header-text">
           <h2>Gustavo Henrique</h2>
-          <span>Desenvolvedor, Artista (nas horas vagas), Cientista da Computação pelo <a href="https://www.ifnmg.edu.br/montesclaros">IFNMG</a>.</span>
+          <span>Desenvolvedor, Artista (nas horas vagas) e Cientista da Computação pelo <a href="https://www.ifnmg.edu.br/montesclaros">IFNMG</a>.</span>
         </div>
       </div>
 
@@ -19,11 +19,11 @@ const AboutMe = () => {
       <div className="about-body">
         
         <div className="topic-section">
-          <h4>Eae, Meu nome é Gustavo! Algumas coisas que eu faço no meu ramo:</h4>
+          <h3>Eae, Meu nome é Gustavo! Essas são algumas coisas que eu faço no meu ramo:</h3>
           <ul>
-            <li>Desenvolvimento, principalmente python e c++, mas já trabalhei com Lua e Golang,</li>
-            <li>Frontend, porém iniciante,</li>
-            <li>Faço pesquisa em problemas de otimização,</li>
+            <li>Desenvolvimento, principalmente Python e C++, mas já trabalhei com Lua e Golang</li>
+            <li>Frontend, porém iniciante</li>
+            <li>Faço pesquisa em problemas de otimização</li>
             <li>Também faço desenhos sob demanda!</li>
           </ul>
         </div>
@@ -31,40 +31,41 @@ const AboutMe = () => {
         <div className="topic-section">
           <h3>Formação</h3>
           <ul>
-            <li>Formei no ensino técnico em Técnico em Informática pelo IFNMG,</li>
-            <li>Atualmente formando no curso Bacharelado em Ciência da Computação também pelo IFNMG.</li>
+            <li>Formei no ensino técnico em Técnico em Informática pelo IFNMG</li>
+            <li>Atualmente formando no curso Bacharelado em Ciência da Computação também pelo IFNMG</li>
           </ul>
         </div>
 
         <div className="topic-section">
           <h3>Outras habilidades</h3>
           <ul>
-            <li>Eu sei inglês bem,</li>
-            <li>Comecei o básico de japonês, mas bem pouquinho.</li>
-            <li>Eu faço desenhos pixelart,</li>
-            <li>Tenho experiência em suporte técnico, validação de conformidade e documentação de projetos.</li>
+            <li>Eu sei inglês bem</li>
+            <li>Comecei o básico de japonês, mas bem pouquinho</li>
+            <li>Tenho experiência em suporte técnico, validação de conformidade e documentação de projetos</li>
+            <li>Eu faço desenhos pixelart</li>
           </ul>
         </div>
 
         <div className="topic-section">
           <h3>Meus interesses pessoais</h3>
           <ul>
-            <li>Jogo vídeo games, gosto bem variado, de JRPG à Puzzle,</li>
-            <li>Leio mangás, vejo alguns animes (principalmente os que são obras de arte visuais),</li>
-            <li>Gosto de desenhar,</li>
-            <li>Gosto de TCGs em geral, MTG, Pokemon, Yugi e FaB!</li>
+            <li>Games, de JRPG a jogos de Puzzle</li>
+            <li>Leio mangás, vejo alguns animes (principalmente os que são obras de arte visuais)</li>
+            <li>Gosto de desenhar</li>
+            <li>Gosto de TCGs em geral, MTG, Pokémon, Yu-gi-oh e FaB!</li>
           </ul>
         </div>
 
         <div className="topic-section">
           <h3>Extra</h3>
           <ul>
-            <li>Meu jogo favorito é o Chrono Cross,</li>
-            <li>Mas tenho um carinho especial pela série Mother e seus semelhantes,</li>
-            <li>Jogos 8 e 16 bit pra mim são os mais charmosos.</li>
-            <li>Amo a estética Western Futurista e fantasia,</li>
-            <li>Gosto muito de animes de comédia,</li>
-            <li>Eu tento ser um cara maneiro (Soul - Soul Eater).</li>
+            <li>Jogo favorito: Chrono Cross</li>
+            <li>Carinho pela franquia Mother</li>
+            <li>Pixelart é meu ponto fraco</li>
+            <li>Amo trilhas sonoras de games</li>
+            <li>Gênero Western Futurista e fantasia são os meus favoritos</li>
+            <li>Gosto muito de animes de comédia</li>
+            <li>Eu tento ser um cara maneiro (Soul - Soul Eater)</li>
           </ul>
         </div>
 
